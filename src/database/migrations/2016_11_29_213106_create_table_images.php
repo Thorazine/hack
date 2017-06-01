76b5d88e9baf8ff39215c656c906e0fe9dedcb20
Thorazine <Thorazine@users.noreply.github.com>
2017-06-01 12:13:13 +0200

    Down migration more friendly

--- src/database/migrations/2016_11_29_213106_create_table_images.php
+++ src/database/migrations/2016_11_29_213106_create_table_images.php
@@ -38,6 +38,6 @@ class CreateTableImages extends Migration
      */
     public function down()
     {
-        Schema::drop('builder_images');
+        Schema::dropIfExists('builder_images');
     }
 }
