a010c2012ebb41e144219702327c00e27cb983da
Matthijs Openneer <matthijs@noprotocol.nl>
2017-06-14 09:46:28 +0200

    altered radio input

--- src/resources/views/cms/input/create/radio.blade.php
+++ src/resources/views/cms/input/create/radio.blade.php
@@ -3,15 +3,14 @@
 		{{ $type['label'] }}
 	</label>
 	<div class="col-sm-9">
-		@foreach(Builder::getArrayValue($model, $type, $type['values'], @$data, $key) as $optionValue => $optionLabel)
+		@foreach(Builder::getArrayValue($model, $type, $type['values'], @$data, $key) as $optionLabel => $optionValue)
 			<div class="radio">
 				<label>
-					{!! Form::radio($key, $optionValue, (@$data[$key] = $optionValue) ? true : (@$type['default'] == $optionValue) ? true : false) !!}
+					{!! Form::radio($key, $optionValue, (@$data[$key] == $optionValue) ? true : (@$type['default'] == $optionValue) ? true : false) !!}
 					{!! $optionLabel !!}
 				</label>
 			</div>
 		@endforeach
 		{!! $errors->first($key, '<p class="text-danger">:message</p>') !!}
-		
 	</div>
 </div>
