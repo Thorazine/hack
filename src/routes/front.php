02d439fce25f8882591b334db772a9f1b25700f5
Matthijs Openneer <matthijs.openneer@gmail.com>
2017-06-30 09:13:05 +0200

    updated front routes and delete gulpfile

--- src/routes/front.php
+++ src/routes/front.php
@@ -11,9 +11,19 @@
 */
 Route::group(['namespace' => 'Thorazine\Hack\Http\Controllers\Front'], function() {
 
+	// Return the sitemap
 	Route::get('sitemap.xml', function() {
 		return response(Storage::disk(config('filesystems.default'))->get('sitemaps/'.Cms::siteId().'/sitemap.xml'), 200)
 			->header('Content-Type', 'text/xml');
+	});
+
+	// Test database connection (pingdom like service)
+	Route::get('polling/database', function() {
+		try {
+		    DB::connection()->getPdo();
+		} catch (\Exception $e) {
+			return response("Could not connect to the database. Please check your configuration.", 500);
+		}
 	});
 
 	// A post route for the form builder
