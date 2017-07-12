3d21c4a796dc8f226a38c6b558aff9e26cff03f7
Matthijs Openneer <matthijs@noprotocol.nl>
2017-07-12 18:02:08 +0200

    smoother scrolling for mobile

--- src/Http/Controllers/Cms/PageController.php
+++ src/Http/Controllers/Cms/PageController.php
@@ -35,7 +35,7 @@ class PageController extends CmsController
         $this->pageable = $pageable;
         $this->notFound = $slug;
         $this->search = $search;
-        // $this->paginateAmount = 1;
+        $this->paginateAmount = 1;
 
         parent::__construct($this);
 
