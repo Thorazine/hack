6b360d98a2cb3c9af5e88e9cab3d7e2fc9caccf2
Matthijs Openneer <matthijs@noprotocol.nl>
2017-07-25 11:56:42 +0200

    Installation tests added

--- src/tests/DatabaseTest.php
+++ src/tests/DatabaseTest.php
@@ -3,14 +3,31 @@
 namespace Thorazine\Hack\Tests\Feature;
 
 use Tests\TestCase;
+use Exception;
+use DB;
 
 class DatabaseTest extends TestCase
 {
     /** 
      * @test
      */
-    public function _test() 
+    public function strict_mode_test() 
     {
-        $this->assertTrue(true, '');
+    	if(config('database.default') == 'mysql') {
+        	$this->assertTrue(! config('database.connections.mysql.strict'), 'Your database has to have strict mode set to false.');
+        }
+    }
+
+    /** 
+     * @test
+     */
+    public function database_connection_test() 
+    {
+    	try {
+		    DB::connection()->getPdo();
+		    $this->assertTrue(true);
+		} catch (Exception $e) {
+			$this->assertTrue(false, 'Could not connect to the database. Please check your configuration.');
+		}
     }
 }
--- src/tests/MailTest.php
+++ src/tests/MailTest.php
@@ -3,14 +3,27 @@
 namespace Thorazine\Hack\Tests\Feature;
 
 use Tests\TestCase;
+use Exception;
+use Mail;
 
 class MailTest extends TestCase
 {
     /** 
      * @test
      */
-    public function _test() 
+    public function mail_send_test() 
     {
-        $this->assertTrue(true, '');
+        try {
+        	// email user for verification to confirm the location
+            Mail::send('hack::emails.test', [], function($message) {
+                $message->to('test@test.nl');
+                $message->subject('Hack CMS - Test mail');
+            });
+
+            $this->assertTrue(true);
+        } 
+        catch (Exception $e) {
+        	$this->assertTrue(false, 'Mail failed with the following response: '.$e->getMessage());
+        }
     }
 }
